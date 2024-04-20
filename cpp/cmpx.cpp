c5971cd74e6377cd26a15bc62d53d6029cbc2c6b
omdxp <omarbelghaouti@gmail.com>
2024-04-20 16:04:16 +0200

    add basic functionalities to cpp library

--- cpp/cmpx.cpp
+++ cpp/cmpx.cpp
@@ -1,6 +1,6 @@
 #include <cmpx>
 
-std::string Complex::stringify()
+std::string Complex::stringify() const
 {
     std::stringstream ss;
     if (this->re == 0 && this->im == 0)
@@ -15,4 +15,110 @@ std::string Complex::stringify()
     else
         ss << this->im << "i )";
     return ss.str();
+}
+
+Complex *Complex::operator+(Complex *other)
+{
+    double re = this->re + other->re;
+    double im = this->im + other->im;
+    return new Complex(re, im);
+}
+
+Complex *Complex::operator-(Complex *other)
+{
+    double re = this->re - other->re;
+    double im = this->im - other->im;
+    return new Complex(re, im);
+}
+
+Complex *Complex::operator*(Complex *other)
+{
+    double re = this->re * other->re - this->im * other->im;
+    double im = this->re * other->im + this->im * other->re;
+    return new Complex(re, im);
+}
+
+Complex *Complex::operator/(Complex *other)
+{
+    Complex *den = *other * other->con();
+    Complex *num = *this * other->con();
+    try
+    {
+        return new Complex(num->re / den->re, num->im / den->re);
+    }
+    catch (const std::exception &e)
+    {
+        throw e;
+    }
+}
+
+bool Complex::operator>(Complex *other) const
+{
+    return this->mod() > other->mod();
+}
+
+bool Complex::operator>=(Complex *other) const
+{
+    return this->mod() >= other->mod();
+}
+
+bool Complex::operator<(Complex *other) const
+{
+    return !(this >= other);
+}
+
+bool Complex::operator<=(Complex *other) const
+{
+    return !(this > other);
+}
+
+bool Complex::operator==(Complex *other) const
+{
+    return (this->re == other->re) && (this->im == other->im);
+}
+
+bool Complex::operator!=(Complex *other) const
+{
+    return !(this == other);
+}
+
+void Complex::operator+=(Complex *other)
+{
+    this->re += other->re, this->im += other->im;
+}
+
+void Complex::operator-=(Complex *other)
+{
+    this->re -= other->re, this->im -= other->im;
+}
+
+void Complex::operator*=(Complex *other)
+{
+    this->re = this->re * other->re - this->im * other->im;
+    this->im = this->re * other->im + this->im * other->re;
+}
+
+void Complex::operator/=(Complex *other)
+{
+    Complex *den = *other * other->con();
+    Complex *num = *this * other->con();
+    try
+    {
+        this->re = num->re / den->re;
+        this->im = num->im / den->re;
+    }
+    catch (const std::exception &e)
+    {
+        throw e;
+    }
+}
+
+double Complex::mod() const
+{
+    return sqrt(pow(this->re, 2) + pow(this->im, 2));
+}
+
+Complex *Complex::con()
+{
+    return new Complex(this->re, -this->im);
 }
--- cpp/example/main.cpp
+++ cpp/example/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <cmpx>
 
+void print_complex(Complex *num)
+{
+    std::cout << num->stringify() << "\n";
+}
+
 int main(void)
 {
-    Complex *number = new Complex(3, -1);
-    std::cout << number->stringify() << "\n";
+    Complex *num = new Complex(3, -1);
+    Complex *other = new Complex(-1, 0);
+    print_complex(num);
+    print_complex(*num * other);
+    if (num == num)
+    {
+        std::cout << "ofc they are equal\n";
+    }
 }
